76ce597855590bc33af74f814d4960074fbd3b99
Shaheen Ghiassy <sghiassy@linkedin.com>
2024-05-29 13:42:02 +0200

    Made click listener

--- app/src/main/java/com/shaheenghiassy/gettingstartedapp/MainActivity.java
+++ app/src/main/java/com/shaheenghiassy/gettingstartedapp/MainActivity.java
@@ -1,12 +1,15 @@
 package com.shaheenghiassy.gettingstartedapp;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RadioButton maleButtonRadioButton = findViewById(R.id.radio_button_male);
+        RadioButton femaleButtonRadioButton = findViewById(R.id.radio_button_female);
+
+        EditText ageEditText = findViewById(R.id.edit_text_age);
+        EditText feetEditText = findViewById(R.id.edit_text_feet);
+        EditText inchesEditText = findViewById(R.id.edit_text_inches);
+        EditText weightEditText = findViewById(R.id.edit_text_weight);
+
+        TextView resultText = findViewById(R.id.text_view_result);
+        resultText.setText("I am the text");
+
+        Button calculateButton = findViewById(R.id.button_calculate);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Wierd", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
 }
