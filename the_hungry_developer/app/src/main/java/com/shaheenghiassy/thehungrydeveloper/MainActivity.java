542fbf75fa14a51fba4b8037a16b341a13b19451
Shaheen Ghiassy <sghiassy@linkedin.com>
2024-05-30 19:28:09 +0200

    Created StartersActivity with Intent

--- the_hungry_developer/app/src/main/java/com/shaheenghiassy/thehungrydeveloper/MainActivity.java
+++ the_hungry_developer/app/src/main/java/com/shaheenghiassy/thehungrydeveloper/MainActivity.java
@@ -1,13 +1,11 @@
 package com.shaheenghiassy.thehungrydeveloper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         startersCard = findViewById(R.id.card_view_starters);
         mainsCard = findViewById(R.id.card_view_mains);
+
+        startersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent starterActivitiesIntent = new Intent(MainActivity.this, StartersActivity.class);
+                startActivity(starterActivitiesIntent);
+            }
+        });
     }
 }
---
+++ the_hungry_developer/app/src/main/java/com/shaheenghiassy/thehungrydeveloper/StartersActivity.java
@@ -0,0 +1,14 @@
+package com.shaheenghiassy.thehungrydeveloper;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starters);
+    }
+}
