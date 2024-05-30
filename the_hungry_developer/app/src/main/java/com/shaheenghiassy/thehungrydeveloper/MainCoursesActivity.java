5bfd383b8e209c3f16d7660174c73deca85b4339
Shaheen Ghiassy <sghiassy@linkedin.com>
2024-05-30 21:09:15 +0200

    Created a new courses activity

---
+++ the_hungry_developer/app/src/main/java/com/shaheenghiassy/thehungrydeveloper/MainCoursesActivity.java
@@ -0,0 +1,15 @@
+package com.shaheenghiassy.thehungrydeveloper;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainCoursesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_courses);
+    }
+}
