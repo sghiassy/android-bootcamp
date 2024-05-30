70fbb327e8448f57a16d664c540be06686569373
Shaheen Ghiassy <sghiassy@linkedin.com>
2024-05-30 19:04:09 +0200

    Installed Sucessfully

--- the_hungry_developer/app/build.gradle.kts
+++ the_hungry_developer/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.shaheenghiassy.thehungrydeveloper"
-        minSdk = 16
+        minSdk = 19
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
