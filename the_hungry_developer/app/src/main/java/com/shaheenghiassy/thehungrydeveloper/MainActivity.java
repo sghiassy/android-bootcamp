8d49a2372687981e7ba30a053043ee03446e849f
Shaheen Ghiassy <sghiassy@linkedin.com>
2024-05-30 19:08:42 +0200

    Made references

--- the_hungry_developer/app/src/main/java/com/shaheenghiassy/thehungrydeveloper/MainActivity.java
+++ the_hungry_developer/app/src/main/java/com/shaheenghiassy/thehungrydeveloper/MainActivity.java
@@ -4,14 +4,21 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView startersCard;
+    CardView mainsCard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startersCard = findViewById(R.id.card_view_starters);
+        mainsCard = findViewById(R.id.card_view_mains);
     }
 }
