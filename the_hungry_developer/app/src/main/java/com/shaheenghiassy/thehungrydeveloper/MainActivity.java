c29abafa6ddd0d032a48656bcdd2e31550fcb0ef
Shaheen Ghiassy <sghiassy@linkedin.com>
2024-05-30 21:30:29 +0200

    Set a click listener for now

--- the_hungry_developer/app/src/main/java/com/shaheenghiassy/thehungrydeveloper/MainActivity.java
+++ the_hungry_developer/app/src/main/java/com/shaheenghiassy/thehungrydeveloper/MainActivity.java
@@ -26,5 +26,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(starterActivitiesIntent);
             }
         });
+
+        mainsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainCoursesIntent = new Intent(MainActivity.this, MainCoursesActivity.class);
+                startActivity(mainCoursesIntent);
+            }
+        });
     }
 }
